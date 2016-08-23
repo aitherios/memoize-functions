2931c9ab847ed7557577f9851434a84a56d45ca0
Renan Mendes Carvalho <renan.carvalho@corp.globo.com>
2016-08-23 10:31:15 -0300

    Implement simple memoization

--- src/__tests__/memoize-functions-test.js
+++ src/__tests__/memoize-functions-test.js
@@ -1,26 +1,71 @@
 import memoizeFunctions from '../memoize-functions'
 
 jest.unmock('../memoize-functions')
+jest.unmock('memoizee')
 
 
 describe('memoizeFunctions()', () => {
   let subject
+  let obj
 
-  beforeEach(() => {
-    subject = {
-      info: () =>
-        ({ output: 'I beg your pardon' }),
-      log: (message = 'Howdy o/') =>
-        ({ output: message }),
-      warn: ({ message = 'Hey!' }) =>
-        ({ output: message }),
-      text:
-        ({ output: 'String' }),
-    }
+  describe('when called without options', () => {
+    beforeEach(() => {
+      obj = {
+        info: () =>
+          ({ output: 'I beg your pardon' }),
+        log: (message = 'Howdy o/') =>
+          ({ output: message }),
+        warn: ({ message = 'Hey!' } = {}) =>
+          ({ output: message }),
+        text:
+          ({ output: 'String' }),
+      }
+      subject = memoizeFunctions(obj)
+    })
+
+    it('returns a new object', () => {
+      expect(subject).not.toBe(obj)
+    })
+
+    it('keeps non function attributes', () => {
+      expect(subject.text).toBe(obj.text)
+      expect(subject.text.output).toBe(obj.text.output)
+    })
+
+    it('memoizes function attributes', () => {
+      expect(subject.info()).toBe(subject.info())
+      expect(subject.log()).toBe(subject.log())
+      expect(subject.warn()).toBe(subject.warn())
+    })
   })
 
-  it('returns a new object', () => {
-    expect(subject).not.toBe(memoizeFunctions(subject))
-    expect(subject.text.output).toBe(memoizeFunctions(subject).text.output)
+  describe('when called specifying attributes to be memoized', () => {
+    beforeEach(() => {
+      obj = {
+        info: () =>
+          ({ output: 'I beg your pardon' }),
+        log: (message = 'Howdy o/') =>
+          ({ output: message }),
+        warn: ({ message = 'Hey!' } = {}) =>
+          ({ output: message }),
+        text:
+          ({ output: 'String' }),
+      }
+      subject = memoizeFunctions(obj, 'text', 'log', 'warn')
+    })
+
+    it('memoizes function specified function attributes', () => {
+      expect(subject.log()).toBe(subject.log())
+      expect(subject.warn()).toBe(subject.warn())
+    })
+
+    it('keeps non function attributes', () => {
+      expect(subject.text).toBe(obj.text)
+      expect(subject.text.output).toBe(obj.text.output)
+    })
+
+    it('does not memoize non specified function attributes', () => {
+      expect(subject.info()).not.toBe(subject.info())
+    })
   })
 })
--- src/memoize-functions.js
+++ src/memoize-functions.js
@@ -1,3 +1,24 @@
-const memoizeFunctions = (obj) => Object.assign({}, obj)
+import memoize from 'memoizee'
+
+const memoizeOpts = {
+  normalizer: (args) => (args[0] ? JSON.stringify(args[0]) : 'id'),
+}
+
+/* eslint-disable no-restricted-syntax */
+
+const memoizeFunctions = (obj, ...attributeNames) => {
+  const memoizedObj = {}
+
+  for (const prop in obj) {
+    if (
+      typeof obj[prop] === 'function'
+      && (attributeNames.length > 0 ? (attributeNames.indexOf(prop) >= 0) : true)
+    ) {
+      memoizedObj[prop] = memoize(obj[prop], memoizeOpts)
+    }
+  }
+
+  return { ...obj, ...memoizedObj }
+}
 
 export default memoizeFunctions
